3e1be64075fff5c3c49e225cfddf696db313ac63
hyper0009 <shehab.ahmed.uk@gmail.com>
2017-07-07 00:35:34 +0100

    creates a header method in printer to print catalogue

--- Console/Printer.cs
+++ Console/Printer.cs
@@ -5,15 +5,17 @@ namespace vendingMachine
 {
     class Printer
     {
-        public string header = "item name | item price" + Environment.NewLine;
+        public string howToSelect = Environment.NewLine + "Please select an item by typing the item's Selection Number:" + Environment.NewLine;
+        public string header = "item name | item price | Selection Number" + Environment.NewLine;
         public void printHeader()
         {
             int length = Vendor.machine.Length;
             for (int i = 0; i < length; i++)
             {
-                header += $"{Vendor.machine[i].name} | {Vendor.machine[i].price}";
+                header += $"{Vendor.machine[i].name} | £{Vendor.machine[i].price} | {i+1}" + Environment.NewLine;
             }
-            Console.WriteLine(header);
+            Console.WriteLine(howToSelect + header);
+            header = "";
         }
     }
 }
--- Console/Program.cs
+++ Console/Program.cs
@@ -4,7 +4,6 @@ namespace vendingMachine
 {
     class Program
     {
-
         public static void Main(string[] args)
         {
             var vendingMachineRunner = new Controller();
